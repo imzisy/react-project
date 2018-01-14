46e7621979ec79c68ba074f3d1600dedda53f9c8
Emad zaibak <Emad zaibak>
2018-01-14 14:10:57 +0800

    fix eslint error for the test

--- __test__/reducers/aReducer.test.js
+++ __test__/reducers/aReducer.test.js
@@ -1,13 +1,12 @@
-import a from "../../src/reducers/aReducer";
+/* eslint-env jest */
+import a from '../../src/reducers/aReducer';
 
-describe("A Reducer", () => {
+describe('A Reducer', () => {
+  it('it has default state', () => {
+    expect(a(undefined, { type: 'unexpected' }).toEqual([]));
+  });
 
-    it("it has default state", () => {
-        expect(a(undefined,{type:"unexpected"})).toEqual([]);
-    });
-
-    it("can handle add A", () => {
-        expect(a(undefined,{type:"ADD_A",payload:"A1"})).toEqual(["A1"]);
-    });
-        
+  it('can handle add A', () => {
+    expect(a(undefined, { type: 'ADD_A', payload: 'A1' })).toEqual(['A1']);
+  });
 });
